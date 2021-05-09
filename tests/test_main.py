6ddb5da73f2f29bdf8c85f2d946f8769eb2401a0
Fraser <fraser.greenlee@mac.com>
2021-05-09 10:50:21 +0100

    add methods

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,13 +1,30 @@
 import unittest
+import snoop
 
-from basic.main import fizzbuzz
+from basic.main import fizzbuzz, ops, loops, make_calls
 
 
 class MyTestCase(unittest.TestCase):
 
+    @snoop(depth=float('inf'))
     def test_fizzbuzz(self):
         s = fizzbuzz(100)
         self.assertEqual(s, 68)
+
+    @snoop(depth=float('inf'))
+    def test_ops(self):
+        s = ops(100, 10)
+        self.assertEqual(s, 90)
+
+    @snoop(depth=float('inf'))
+    def test_loops(self):
+        s = loops()
+        self.assertEqual(s, 9)
+
+    @snoop(depth=float('inf'))
+    def test_calls(self):
+        s = make_calls()
+        self.assertEqual(s, None)
 
 
 if __name__ == '__main__':
